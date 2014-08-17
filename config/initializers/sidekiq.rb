cb983a8787bc98b3460f4f5e3e3109e309d1be47
Will Fleming <will@mintdigital.com>
2014-08-16 23:07:18 -0400

    fix sidekiq redis setup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,18 @@
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  config.redis = {
+    url: ENV["REDISCLOUD_URL"],
+    namespace: 'sidekiq',
+    size: 1
+  }
 end
 
 # https://devcenter.heroku.com/articles/forked-pg-connections#sidekiq
 Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV["REDISCLOUD_URL"],
+    namespace: 'sidekiq',
+  }
+  
   database_url = ENV['DATABASE_URL']
   if database_url
     ENV['DATABASE_URL'] = "#{database_url}?pool=25"
